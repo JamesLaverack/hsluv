8ddfe58eec386787bab5b786247c50fd25b80392
James Laverack <james@jameslaverack.com>
2017-10-22 23:00:27 +0100

    Project setup

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to hsluv
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/hsluv/core_test.clj
@@ -0,0 +1,7 @@
+(ns hsluv.core-test
+  (:require [clojure.test :refer :all]
+            [hsluv.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
