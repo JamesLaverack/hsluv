bce7488dee9cb9afce6e81979045a6774ef4bcc1
James Laverack <james@jameslaverack.com>
2017-11-04 01:04:16 +0000

    Use snapshot data for testing

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [net.mikera/core.matrix "0.61.0"]
-                 [org.clojure/math.numeric-tower "0.0.4"]])
+                 [org.clojure/math.numeric-tower "0.0.4"]]
+  :profiles {:test {:dependencies [[org.clojure/data.json "0.2.6"]]}})
--- test/hsluv/core_test.clj
+++ test/hsluv/core_test.clj
@@ -1,11 +1,17 @@
 (ns hsluv.core-test
   (:require [clojure.test :refer :all]
+            [clojure.java.io :as io]
+            [clojure.data.json :as json]
             [hsluv.core :refer :all]))
-
-(deftest matrix-operations
-  (testing "That we can use matrix operations"
-    (is (= [2 2] (foo)))))
 
 (deftest rgb-to-xyz-test
   (testing "sRGB to CIE XYZ converstion"
-    (is (= [0.308043578886299796 0.612655858810891907 0.102019012460713238] (rgb-to-xyz [0.0666666666666666657 0.933333333333333348 0])))))
+    (let [data (json/read (io/reader (io/resource "snapshot-rev4.json")))]
+      (loop [colour (first (vals data))
+             remaining-colours (rest (vals data))]
+        (if (not (nil? colour))
+          (let [rgb (get colour "rgb")
+                xyz (get colour "xyz")]
+            (is (= xyz (rgb-to-xyz rgb)))
+            (recur (first remaining-colours) (rest remaining-colours))))))))
+      
