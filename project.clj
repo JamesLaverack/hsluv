614f641c64497d2191f789186a86c50eb5579416
James Laverack <james@jameslaverack.com>
2018-01-28 22:49:27 +1100

    Correct project file

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject hsluv "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "Pure Clojure implementation of HSLuv"
+  :url "https://github.com/JamesLaverack/hsluv"
+  :license {:name "MIT"
+            :url "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [net.mikera/core.matrix "0.61.0"]
                  [org.clojure/math.numeric-tower "0.0.4"]]
